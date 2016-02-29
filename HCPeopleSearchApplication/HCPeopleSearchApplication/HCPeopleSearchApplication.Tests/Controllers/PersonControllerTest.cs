487537bca0bde1acd6d3012a88322fa90fa3baff
Arun <arunprasanna.k@gmail.com>
2016-02-29 00:27:20 -0700

    First Commit

---
+++ HCPeopleSearchApplication/HCPeopleSearchApplication/HCPeopleSearchApplication.Tests/Controllers/PersonControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using HCPeopleSearchApplication.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HCPeopleSearchApplication.Tests.Controllers
+{
+    [TestClass]
+    public class PersonControllerTest
+    {
+
+        //Methdo to Test, whether Index View is returning 
+        [TestMethod]
+        public void Index()
+        {
+            PersonController controller = new PersonController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        //Method to test Whether proper Details are retreiving by Json
+        //This'll Should fail, as It can't talk to Database in EF Code First.
+        [TestMethod]
+        public void GetPersonDetails()
+        {
+            PersonController controller = new PersonController();
+            
+            var result=controller.GetPersonDetails("Arun") as JsonResult;
+
+            var data = result.Data;
+
+            Assert.IsNotNull(data,"No JsonResult returned from action method");
+           // Assert.IsNotNull(result);
+
+        }
+
+        //Method to test, Create Index is returning
+        [TestMethod]
+        public void Create()
+        {
+            PersonController controller = new PersonController();
+            ViewResult result= controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }      
+        
+    }
+}
